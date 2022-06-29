8ea6516fea2c94a9ae2eaa1626e90377d096e9a0
amir ramezanpour <rp.amir94@gmail.com>
2022-06-29 09:58:39 +0430

    video embed problem fixed

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -148,11 +148,13 @@ ClassicEditor.defaultConfig = {
 					const id = match[ 1 ];
 
 					return (
-						'<div style="position: relative; padding-bottom: 100%; height: 0; padding-bottom: 56.2493%;">' +
-							`<iframe src="https://www.aparat.com/video/video/embed/videohash/${ id }/vt/frame" ` +
-								'style="position: absolute; width: 100%; height: 100%; top: 0; left: 0;" ' +
-								'frameborder="0" allow="autoplay; encrypted-media" allowfullscreen>' +
-							'</iframe>' +
+						'<div style="max-width: 600px;margin: auto;border-radius: 5px;overflow: hidden;">' +
+							'<div style="position: relative; padding-bottom: 100%; height: 0; padding-bottom: 56.2493%;">' +
+								`<iframe src="https://www.aparat.com/video/video/embed/videohash/${ id }/vt/frame" ` +
+									'style="position: absolute; width: 100%; height: 100%; top: 0; left: 0;" ' +
+									'frameborder="0" allow="autoplay; encrypted-media" allowfullscreen>' +
+								'</iframe>' +
+							'</div>' +
 						'</div>'
 					);
 				}
